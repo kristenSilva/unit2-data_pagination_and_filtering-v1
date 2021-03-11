29e3d381a637d0480fe9f7071a9f24f844af39d2
kristenSilva <kristensilvarobles@gmail.com>
2021-03-11 12:45:51 -0800

    addPagination function added
    project meets expectations

--- js/script.js
+++ js/script.js
@@ -3,25 +3,23 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-
-
 /*
 For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
-
+const perPage = 9;
 
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 list param = student data
-page param = page number
+page param = page number which will determine the 9 students selected from list
 */
 function showPage(list, page){
-   const startIndex = (page*9) - 9;
-   const endIndex = page*9;
+   const startIndex = (page*perPage) - perPage;
+   const endIndex = page*perPage;
    const ulStudentList = document.querySelector('.student-list');
    ulStudentList.innerHTML = '';
 
@@ -40,17 +38,45 @@ function showPage(list, page){
          </li>`);
       }
    }
+   console.log(startIndex);
+   console.log(endIndex);
 }
-showPage(data, 1);
-
-
 
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
+list param = student data
 */
+function addPagination(list){
+   const numButtons = Math.ceil(list.length/perPage);
+   const linkList = document.querySelector('.link-list');
+   linkList.innerHTML = '';
 
+   for(let i = 1; i < numButtons+1; i++){
+      linkList.insertAdjacentHTML('beforeend',
+         `<li>
+            <button type="button">${i}</button>
+         </li>`);
+   }
+
+   const firstButton = linkList.firstElementChild.firstElementChild;
+   firstButton.className = 'active';
+
+   linkList.addEventListener('click', (e) => {
+      if(e.target.tagName ==='BUTTON'){
+         const activeButtons = document.getElementsByClassName('active');
+         for(let i = 0; i < activeButtons.length; i++){
+            activeButtons[i].classList.remove('active');
+         }
+         e.target.className= 'active';
+         showPage(list, e.target.textContent);
+         console.log(e.target.textContent);
+      }
+   });
+}
 
 
 // Call functions
+showPage(data, 1);
+addPagination(data);
